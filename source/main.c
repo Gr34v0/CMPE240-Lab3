2c09d446fe19649b8c803a135f4b86fabc214eed
Gr34v0 <gr34v0@gmail.com>
2017-04-10 21:55:55 -0400

    final

--- source/main.c
+++ source/main.c
@@ -8,13 +8,10 @@ void blink_once()
     // GPIO3 bits 11-9  bit 001 = output
     // Turn LED on
     gpio[GPSET0] |= (1 << 3);
-
     timer_delay_sec(1);
-
     //toggle set register for the chosen pin
     // Turn LED off
     gpio[GPCLR0] |= (1 << 3);
-    
 }
 
 //On for one second, off for 5 seconds, on for one second......
@@ -24,14 +21,10 @@ void blink_code(uint32_t err)
     {
         // Blink the LED
         blink_once();
-
         timer_delay_sec(1);
-
     }
-
     // Delay for desired time
     timer_delay_sec(5);
-    
 }
 
 int main()
@@ -39,32 +32,23 @@ int main()
     // Init GPIO select for external LED
     //GPIO3
     gpio[GPFSEL0] = (1 << 9);
-
     // Init uart for debugging purposes
     init_uart();
-
     // Provide a buffer size for debug prints
     uint32_t buffer_size = 80;
-
     // You might need a count for something
     uint32_t count = 1;
-
     // Mainline loop
     while (1)
     {
         count = count % 10;
         // implement error code described in lab
-
         if(count != 0){
-
             //call blink_code at some point
             //error code has to match 900000 if lab 1 was right
             blink_code(count);
-
-        }
-        
+        } 
         ++count;
     }
-    
     return 0;
 }
--- source/system_timer.c
+++ source/system_timer.c
@@ -2,26 +2,21 @@
 
 #define checkTimerMatch(bit) ((sys_timer[SYS_TIMER_CS] & (1 << bit)) == 0 ) //Returns True when bit at "bit"
                                                                             // is 1
-
 void timer_delay_us(uint32_t delayUs)
 {
     //Page 172 of manual
     // First, get the current count of the timer.
     uint32_t x = sys_timer[SYS_TIMER_CLO];
-
     // Next, add the delay to the current count.
     // and, put that into the C0 timer register;
     x = x + delayUs;
     sys_timer[SYS_TIMER_C0] = x;
-
     // Clear the M0 timer register.
     sys_timer[SYS_TIMER_CS] &= (0 << 0);
-    
-
     // Now we spin until the CS register
     // has a 1 in the 0 position, then we know
     // our timer has expired.
-    while(checkTimerMatch(0)){
+    while(checkTimerMatch(0)){ //0th bit for the macro defined above
         //NOOP
     }
 }
@@ -30,12 +25,10 @@ void timer_delay_ms(uint32_t delayMs)
 {
     // scale the microsecond delay appropriately
     timer_delay_us(1000 * delayMs);
-    
 }
 
 void timer_delay_sec(uint32_t delaySec)
 {
     // scale the microsecond delay appropriately
-    timer_delay_us(1000000 * delaySec);
-    
+    timer_delay_us(1000000 * delaySec); 
 }
